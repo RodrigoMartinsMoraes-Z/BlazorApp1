35fc905f798981f7d1103f1c8c02e36922bc0b0c
Rodrigo Martins Moraes <rodrigo_moraes@hotmail.com.br>
2019-11-14 14:45:58 -0300

    mudou tudo e nao mudou nada

--- BlazorApp1/Scripts/Auxiliar.cs
+++ BlazorApp1/Scripts/Auxiliar.cs
@@ -1,5 +1,7 @@
 ﻿using BlazorInputFile;
 using System;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Threading.Tasks;
 
@@ -16,8 +18,30 @@ namespace BlazorApp1.Scripts
                 bytes = memoryStream.ToArray();
             }
 
-            string base64 = Convert.ToBase64String(bytes);
-            return base64;
+            string imageBase64String = Convert.ToBase64String(bytes);
+
+            byte[] imageBytes = Convert.FromBase64String(imageBase64String);
+            MemoryStream imageStream = new MemoryStream(imageBytes, 0, imageBytes.Length);
+            imageStream.Write(imageBytes, 0, imageBytes.Length);
+            Image img = Image.FromStream(imageStream, true);
+
+            //
+            // Convert Image to Graphics Object
+            Graphics graphics = Graphics.FromImage(img);
+            graphics.RotateTransform(90);
+            
+            //
+            //Convert Graphics Object to Base64String
+            using (MemoryStream ms = new MemoryStream())
+            {
+                // Convert Image to byte[]
+                img.Save(ms, ImageFormat.Jpeg);
+                imageBytes = ms.ToArray();
+
+                // Convert byte[] to Base64 String
+                string base64String = Convert.ToBase64String(imageBytes);
+                return base64String;
+            }
         }
     }
 }
