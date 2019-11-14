b9b693ce11b0c1daade7e11e99a808faa8a3f252
Rodrigo Martins Moraes <rodrigo_moraes@hotmail.com.br>
2019-11-14 13:49:57 -0300

    Add project files.

---
+++ BlazorApp1/Scripts/Auxiliar.cs
@@ -0,0 +1,23 @@
+﻿using BlazorInputFile;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Scripts
+{
+    public class Auxiliar
+    {
+        public static async Task<string> ImagemEmByte(IFileListEntry arquivo)
+        {
+            byte[] bytes;
+            using (var memoryStream = new MemoryStream())
+            {
+                await arquivo.Data.CopyToAsync(memoryStream);
+                bytes = memoryStream.ToArray();
+            }
+
+            string base64 = Convert.ToBase64String(bytes);
+            return base64;
+        }
+    }
+}
