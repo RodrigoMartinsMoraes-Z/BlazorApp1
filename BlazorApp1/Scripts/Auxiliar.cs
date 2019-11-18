1b14f262342e9014a654acc03910ba6bbb51e19c
Rodrigo Martins Moraes <rodrigo_moraes@hotmail.com.br>
2019-11-18 14:44:24 -0300

    rotacionar imagem c#

--- BlazorApp1/Scripts/Auxiliar.cs
+++ BlazorApp1/Scripts/Auxiliar.cs
@@ -11,42 +11,8 @@ namespace BlazorApp1.Scripts
     {
         public static async Task<string> ArquivoEmBase64(IFileListEntry arquivo)
         {
-            #region rotacionarImagem
-            //byte[] bytes;
-            //using (var memoryStream = new MemoryStream())
-            //{
-            //    await arquivo.Data.CopyToAsync(memoryStream);
-            //    bytes = memoryStream.ToArray();
-            //}
-
-            //string imageBase64String = Convert.ToBase64String(bytes);
-
-            //byte[] imageBytes = Convert.FromBase64String(imageBase64String);
-            //MemoryStream imageStream = new MemoryStream(imageBytes, 0, imageBytes.Length);
-            //imageStream.Write(imageBytes, 0, imageBytes.Length);
-            //Image img = Image.FromStream(imageStream, true);
-
-            ////
-            //// Convert Image to Graphics Object
-            //Graphics graphics = Graphics.FromImage(img);
-            //graphics.RotateTransform(90);
-
-            ////
-            ////Convert Graphics Object to Base64String
-            //using (MemoryStream ms = new MemoryStream())
-            //{
-            //    // Convert Image to byte[]
-            //    img.Save(ms, ImageFormat.Jpeg);
-            //    imageBytes = ms.ToArray();
-
-            //    // Convert byte[] to Base64 String
-            //    string base64String = Convert.ToBase64String(imageBytes);
-            //    return base64String;
-            //}
-            #endregion
-
             byte[] bytes;
-            
+
             using (var memoryStream = new MemoryStream())
             {
                 await arquivo.Data.CopyToAsync(memoryStream);
@@ -55,6 +21,24 @@ namespace BlazorApp1.Scripts
 
             string base64 = Convert.ToBase64String(bytes);
             return base64;
+        }
+
+        public static string GirarImagem(string imagemBase64)
+        {
+            byte[] imageBytes = Convert.FromBase64String(imagemBase64);
+            MemoryStream imageStream = new MemoryStream(imageBytes, 0, imageBytes.Length);
+            imageStream.Write(imageBytes, 0, imageBytes.Length);
+            Image img = Image.FromStream(imageStream, true);
+            img.RotateFlip(RotateFlipType.Rotate90FlipNone);
+
+            using MemoryStream ms = new MemoryStream();
+            // Convert Image to byte[]
+            img.Save(ms, ImageFormat.Jpeg);
+            imageBytes = ms.ToArray();
+
+            // Convert byte[] to Base64 String
+            string base64String = Convert.ToBase64String(imageBytes);
+            return base64String;
         }
     }
 }
